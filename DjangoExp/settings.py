1a0c60ef307491d02c43d0fa887f33c41b4c5b1c
mhaselsteiner <magdalena.haselsteiner@gmx.at>
2018-08-12 10:56:08 +0200

    change hideaway of secret key

--- DjangoExp/settings.py
+++ DjangoExp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from key import key_variable
+from key import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-SECRET_KEY = key_variable
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
